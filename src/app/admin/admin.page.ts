19cb97d07da8215b4982a0a77be10cd5cfee3117
jwk24 <jwk24@maroon22.cs.calvin.edu>
2020-02-06 11:15:04 -0500

    Data Table comlete, added dummy upload button

--- src/app/admin/admin.page.ts
+++ src/app/admin/admin.page.ts
@@ -21,30 +21,58 @@ export class AdminPage implements OnInit {
   public rows: any;
   constructor(private rankingService : RankingService, private http : HttpClient) {
     this.columns = [
-      { name: 'Name' },
-      { name: 'Title' },
-      { name: 'Rating' }
+      { prop: 'title', name: 'Title' },
+      { prop: 'avgrating', name: 'Average Rating' },
+      { prop: 'percent', name: 'Percentage of 3\'s'},
+      { prop: 'sum', name: 'Number of 3\'s'}
     ];
-    this.http.get<Data>('../../assets/movies.json')
-      .subscribe((res) => {
-        console.log(res)
-        this.rows = res.movies;
+    this.rankingService.getRankings().subscribe((rankings) => {
+      console.log(rankings);
+      let uniqueTitles = this.getUniqueTitles(rankings);
+      this.rows = uniqueTitles.map(bookTitle => {
+        return {
+          title : bookTitle,
+          avgrating : this.calculateAverageRatingPerTitle(rankings, bookTitle),
+          percent : this.calculatePercentagePerScore(rankings, bookTitle, 3),
+          sum : this.calculateSumPerScore(rankings, bookTitle, 3)
+        }
       });
-      this.rankingService.getRankings().subscribe((rankings) => {
-        console.log(rankings);
-        this.rows = rankings.map(ranking => {
-          return {
-            name : ranking.lastName + ", " + ranking.firstName,
-            title : ranking.bookTitle,
-            rating : ranking.score
-          }
-        });
-        console.log(this.rows);
-      });
+      console.log(this.rows);
+    });
   }
 
   ngOnInit() {
     
+  }
+
+  getUniqueTitles(rankings : Ranking[]) {
+    let titles = [];
+    rankings.forEach(ranking => {
+      if(!titles.includes(ranking.bookTitle)) {
+        titles.push(ranking.bookTitle);
+      }
+    });
+    return titles;
+  }
+
+  calculateAverageRatingPerTitle(rankings : Ranking[], title : String) : String {
+    let allRatingsForThisTitle = rankings.filter(ranking => ranking.bookTitle == title); // Only ratings with this title
+    return allRatingsForThisTitle.reduce((total, ranking) => total + ranking.score / allRatingsForThisTitle.length, 0).toFixed(2); // Average those ratings
+  }
+
+  calculatePercentagePerScore(rankings : Ranking[], title : String, score : number) {
+    let scoreFilteredList = rankings.filter(ranking => { // Only ratings with a certain score and title
+      return ranking.score == score && ranking.bookTitle == title; 
+    });
+    let allRatingsForThisTitle = rankings.filter(ranking => ranking.bookTitle == title);
+    return scoreFilteredList.length / allRatingsForThisTitle.length * 100   + "%" // Number out of 100 of the total number of ratings for that title
+  }
+
+  calculateSumPerScore(rankings : Ranking[], title : String, score : number) {
+    let scoreFilteredList = rankings.filter(ranking => { // Only ratings with a certain score and title
+      return ranking.score == score && ranking.bookTitle == title; 
+    });
+    return scoreFilteredList.length;
   }
 
 }
